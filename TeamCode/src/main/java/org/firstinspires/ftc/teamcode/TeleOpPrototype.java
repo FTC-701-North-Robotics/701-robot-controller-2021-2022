077a8c90f782a2834ee545b43d93abbb67c32168
Kennan Hunter <kennanhunter5@gmail.com>
2021-09-18 21:53:42 -0400

    removed comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpPrototype.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpPrototype.java
@@ -2,27 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- * <p>
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- * <p>
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @TeleOp(name = "TeleOpPrototype", group = "Linear Opmode")
-@Disabled
 public class TeleOpPrototype extends LinearOpMode {
 
     // Declare OpMode members.
@@ -32,45 +18,32 @@ public class TeleOpPrototype extends LinearOpMode {
     private DcMotor lfDrive = null;
     private DcMotor rfDrive = null;
 
-
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        // Initialize the hardware variables. Note that the strings used here as parameters
-        // to 'get' must correspond to the names assigned during the robot configuration
-        // step (using the FTC Robot Controller app on the phone).
         lbDrive = hardwareMap.get(DcMotor.class, "lbdrive");
         rbDrive = hardwareMap.get(DcMotor.class, "rbdrive");
         lfDrive = hardwareMap.get(DcMotor.class, "lfdrive");
         rfDrive = hardwareMap.get(DcMotor.class, "rfdrive");
 
-        // Most robots need the motor on one side to be reversed to drive forward
-        // Reverse the motor that runs backwards when connected directly to the battery
         lbDrive.setDirection(DcMotor.Direction.FORWARD);
         lfDrive.setDirection(DcMotor.Direction.FORWARD);
         rbDrive.setDirection(DcMotor.Direction.REVERSE);
         rfDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            // Setup a variable for each drive wheel to save power level for telemetry
             double lbDrivePower;
             double rbDrivePower;
             double lfDrivePower;
             double rfDrivePower;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
-
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
             double strafe = gamepad1.left_stick_x;
@@ -79,13 +52,12 @@ public class TeleOpPrototype extends LinearOpMode {
             rbDrivePower = (Range.clip((drive - turn) * strafe, -1.0, 1.0));
             lfDrivePower = (Range.clip((drive + turn) * -strafe, -1.0, 1.0));
             rfDrivePower = (Range.clip((drive - turn) * -strafe, -1.0, 1.0));
+
             lbDrive.setPower(lbDrivePower);
             rbDrive.setPower(rbDrivePower);
             lfDrive.setPower(lfDrivePower);
             rfDrive.setPower(rfDrivePower);
 
-
-            // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left back (%.2f), right back (%.2f), left front (%.2f), right front (%.2f)", lbDrivePower, rbDrivePower, lfDrivePower, rfDrivePower);
             telemetry.update();
